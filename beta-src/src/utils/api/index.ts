f9eefee6afe1ef07240654c34ed9fadbe4556288
Joel Quiles <quilesbaker@gmail.com>
2022-09-19 13:53:08 -0400

    Adding notes on how we'll grab the cognito token/user data. We should likely use aws-amplify helpers if we go this path. For now, we'll use the existing authentication.

--- beta-src/src/utils/api/index.ts
+++ beta-src/src/utils/api/index.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from "axios";
+// import { API, Auth } from "aws-amplify";
 import ApiRoute from "../../enums/ApiRoute";
 
 export type QueryParams = {
@@ -16,12 +17,41 @@ const buildQueryString = (params: QueryParams): string =>
     }, [])
     .join("&");
 
+
+// TODO We'll actually change this to a promise at runtime using API/Auth, but for now need o figure this out...
+// Should work once user authenticated, but not when starting from logged out state
+// we'll actually probably use the jwt token as api key... as thats not permanent like user id
+const lastUserId = localStorage.getItem("CognitoIdentityServiceProvider.4eok31qvrn3a9p7sadlqhis6cl.LastAuthUser");
+
+// TODO add Authorization header using bearer from AWS or so in order to
+// compare to API key in database to make requests, once authenticated
 const api = axios.create({
   // why do we need multipart form-data?
-  // headers: {
-  //   "Content-Type": "multipart/form-data",
-  // },
+  headers: {
+    Authorization: `Bearer ${lastUserId}`
+    // "Content-Type": "multipart/form-data",
+  },
 });
+
+// async function getAuthAPIHeaders() {
+//   const user = await Auth.currentAuthenticatedUser();
+
+//   // TODO we really only need the API key, not the jwt token
+//   const token = user.signInUserSession.idToken.jwtToken;
+
+//   // TODO hardcoded for now
+//   const API_KEY = "";
+
+//   const requestInfo = {
+//     headers: {
+//       Authorization: `Bearer ${API_KEY}`
+//     }
+//   };
+
+//   return requestInfo;
+
+// }
+
 
 export const getGameApiRequest = (
   route: ApiRoute,
@@ -30,6 +60,7 @@ export const getGameApiRequest = (
 ): Promise<AxiosResponse> =>
   api.get(`../api.php?route=${route}&${buildQueryString(queryParams)}`, {
     timeout,
+    // ...getAuthAPIHeaders() // TODO this returns a promise right now can piggyback on axios promise returned
   });
 
 export const postGameApiRequest = (
@@ -39,11 +70,13 @@ export const postGameApiRequest = (
 ): Promise<AxiosResponse> =>
   api.post(`../api.php?route=${route}`, json, {
     timeout,
+    // ...getAuthAPIHeaders() // TODO this returns promise.. check
   });
 
 const orderSubmission = axios.create({
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: `Bearer ${lastUserId}`
   },
 });
 
@@ -56,7 +89,9 @@ export const submitOrders = (
     return orderSubmission.post(
       `../ajax.php?${buildQueryString(queryParams)}`,
       orders,
+      // getAuthAPIHeaders()
     );
   }
-  return orderSubmission.post(`../ajax.php`, orders);
+  return orderSubmission.post(`../ajax.php`, orders, // getAuthAPIHeaders()
+                             );
 };
