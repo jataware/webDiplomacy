c01fb704a0b62e53a83decdd69dd1e8fa62e05ae
Joel Quiles <quilesbaker@gmail.com>
2022-10-17 13:23:56 -0400

    Updated aws-exports-dev con include new IRB consent API changes for dev deployment

--- beta-src/dev/aws-exports-dev.js
+++ beta-src/dev/aws-exports-dev.js
@@ -25,6 +25,13 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
+    ],
+    "aws_cloud_logic_custom": [
+      {
+        "name": "webDiplomacyExtAPI",
+        "endpoint": "https://4a3dkart1b.execute-api.us-east-1.amazonaws.com/dev",
+        "region": "us-east-1"
+      }
     ]
 };
 
